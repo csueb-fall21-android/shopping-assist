afd272b5682a2e6934e96a624750c3e969733ea8
Sharon Wong <26sswong@gmail.com>
2021-10-28 20:43:32 -0700

    12: bottom navigation and fragments creation

---
+++ app/src/main/java/com/shoppingassist/fragments/ProfileFragment.java
@@ -0,0 +1,69 @@
+package com.shoppingassist.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+import com.shoppingassist.LoginActivity;
+import com.shoppingassist.MainActivity;
+import com.shoppingassist.R;
+
+public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
+    private Button btnLogout;
+
+    public interface LogoutUserListener {
+        void onLogoutUser();
+    }
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick logout");
+
+                LogoutUserListener listener = (LogoutUserListener) getActivity();
+                listener.onLogoutUser();
+            }
+        });
+    }
+}
