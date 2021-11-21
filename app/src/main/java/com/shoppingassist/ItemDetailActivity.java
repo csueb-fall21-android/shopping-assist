39074fd01d82e75910510331d5febec3657a104e
hakusone <26sswong@gmail.com>
2021-11-20 16:00:03 -0800

    hooked up search activity to view details (#86)

--- app/src/main/java/com/shoppingassist/ItemDetailActivity.java
+++ app/src/main/java/com/shoppingassist/ItemDetailActivity.java
@@ -4,24 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.parse.GetDataCallback;
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.shoppingassist.models.Item;
 
 public class ItemDetailActivity extends AppCompatActivity {
     private static final String TAG = "ItemDetailActivity";
     private Context context;
-    
+    private Item item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_details);
@@ -32,39 +37,52 @@ public class ItemDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
 
-        Bundle extras = getIntent().getExtras();
-        ParseFile imgFile = (ParseFile)extras.get("vDetails");
-
-        String prodNameVal = extras.getString("prodName");
-        Number priceTextVal = extras.getInt("priceText");
-        String locTextVal = extras.getString("locText");
+        item = getIntent().getParcelableExtra("item");
 
         ImageView vDetails = findViewById(R.id.vDetails);
         TextView prodName = findViewById(R.id.prodName);
         TextView priceText = findViewById(R.id.priceText);
         TextView locText = findViewById(R.id.locText);
 
-        imgFile.getDataInBackground(new GetDataCallback() {
+        ParseFile image = item.getPictureFile();
+
+        if (image != null) {
+            Glide.with(context).load(image.getUrl()).into(vDetails);
+        }
+
+        prodName.setText(item.getName());
+        priceText.setText(String.valueOf(item.getPrice()));
+        locText.setText(item.getLocation());
+
+        FloatingActionButton fab = findViewById(R.id.fabSearch);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(byte[] data, ParseException e) {
-                if (e == null) {
-                    // Decode the Byte[] into Bitmap
-                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-                    // Set the Bitmap into the ImageView
-                    vDetails.setImageBitmap(bmp);
-                } else {
-                    Log.e(TAG,"Error loading image from data: " + e.getMessage());
-                }
+            public void onClick(View view) {
+                beginSearch();
             }
         });
-        prodName.setText(prodNameVal);
-        priceText.setText(String.valueOf(priceTextVal));
-        locText.setText(locTextVal);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+
+    private void beginSearch() {
+        startSearchActivity(item.getName(), item);
+    }
+
+    /**
+     * Used to begin a search on a specific query
+     * By default, this is hooked up to the Placeholder Search API, and not the real Serp Search API
+     */
+    private void startSearchActivity(String query, Item item) {
+        Intent intent = new Intent(ItemDetailActivity.this, SearchActivity.class);
+        intent.putExtra("query", query);
+        intent.putExtra("item", item);
+
+        ItemDetailActivity.this.startActivity(intent);
     }
 }
